d40c8dae5260ad7504891776a5ce88c55b6972e0
SakethDodda12 <sakethdodda57@gmail.com>
2025-10-31 20:17:03 -0500

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,11 @@
+class IOSString():
+    def __init__(self):
+        self.str1 = ""
+    def get_String(self):
+        self.str1 = input("Enter string:")
+    def print_String(self):
+        print("Result is :", self.str1.upper())
+str1 = IOSString()
+
+str1.get_String()
+str1.print_String()
